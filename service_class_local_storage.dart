5e1c32e4d8e1695c7467ec3183a82c376e29b24c
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2023-03-31 17:31:41 +0200

    Add remove, int and bool functions

--- service_class_local_storage.dart
+++ service_class_local_storage.dart
@@ -1,14 +1,39 @@
 import 'package:shared_preferences/shared_preferences.dart';
 class LocalStorageService {
 
-  static Future<void> saveData({required String name, required String key}) async {
+  static Future<void> saveIntData({required int value, required String key}) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(key, name);
+    await prefs.setInt(key, value);
   }
 
-  static Future<String> getData({required String key}) async {
+  static Future<int> getIntData({required String key}) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(key) ?? '';
+  }
+  
+  static Future<void> saveStringData({required String value, required String key}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
+  }
+
+  static Future<String> getStringData({required String key}) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(key) ?? '';
+  }
+  
+  static Future<void> saveBoolData({required bool value, required String key}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(key, value);
+  }
+
+  static Future<bool> getBoolData({required String key}) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(key) ?? '';
+  }
+  
+  static Future<void> removeData({required String key}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
   }
 
 }
