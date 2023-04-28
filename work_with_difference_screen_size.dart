a39b99c8e6efc0b6e2d18de3649e0ae86a7d0d3d
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2023-04-28 18:23:39 +0200

    Create work_with_difference_screen_size.dart

---
+++ work_with_difference_screen_size.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class InfoDeviceConstants {
+  BuildContext context;
+  InfoDeviceConstants(this.context) : assert (context != null);
+  double get shortestSide => MediaQuery.of(context).size.shortestSide;
+  Enum get deviceOrentation => MediaQuery.of(context).orientation;
+
+
+  bool isMobile() {
+    if (shortestSide < 600){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  bool isTabletLandscape() {
+    if (deviceOrentation == Orientation.landscape){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  void portraitModeOnly() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  void allModeOnly() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+
+  void landscapeModeOnly() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+}
