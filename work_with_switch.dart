db62d4655ca0baa3532c622437bdbab31e23e139
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2023-05-10 10:21:38 +0200

    Create work_with_switch.dart

---
+++ work_with_switch.dart
@@ -0,0 +1,19 @@
+List<String> fruits = ['apple', 'banana', 'cherry', 'date'];
+int index = 2;
+
+switch (fruits[index]) {
+  case 'apple':
+    print('You chose an apple');
+    break;
+  case 'banana':
+    print('You chose a banana');
+    break;
+  case 'cherry':
+    print('You chose a cherry');
+    break;
+  case 'date':
+    print('You chose a date');
+    break;
+  default:
+    print('Invalid fruit');
+}
