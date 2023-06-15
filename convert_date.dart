4d49cf18732e340452e908748e4ff4a6e2145115
Giovanni Tarallo <109211784+Giosk27@users.noreply.github.com>
2023-06-15 15:19:07 +0200

    Update convert_date.dart
    update

--- convert_date.dart
+++ convert_date.dart
@@ -1,2 +1,2 @@
-
-      String date = ('${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')}T${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}Z');
+String formattedDateTime =
+          DateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(dateTime);
