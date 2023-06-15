0036bf6e86cf95333f598db201b4bf26f268076d
Giovanni Tarallo <109211784+Giosk27@users.noreply.github.com>
2023-06-15 12:46:38 +0200

    Create convert_date.dart

---
+++ convert_date.dart
@@ -0,0 +1,2 @@
+
+      String date = ('${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')}T${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}Z');
