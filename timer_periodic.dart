fdbf7d3e0c6e0762678a5a8a61048002e23b2dc0
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2023-07-02 10:26:24 +0200

    Create timer_periodic.dart

---
+++ timer_periodic.dart
@@ -0,0 +1,14 @@
+var counter = 3;
+Timer.periodic(const Duration(seconds: 2), (timer) {
+  print(timer.tick);
+  counter--;
+  if (counter == 0) {
+    print('Cancel timer');
+    timer.cancel();
+  }
+});
+// Outputs:
+// 1
+// 2
+// 3
+// "Cancel timer"
