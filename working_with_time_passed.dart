6eb893090d26f206b9218c154921af979cd4e688
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2023-07-14 07:08:32 +0200

    Refactor with getter

--- working_with_time_passed.dart
+++ working_with_time_passed.dart
@@ -20,3 +20,26 @@ String getNewsTime() {
 
   return time;
 }
+
+//If you don't have parameters you can use the getter
+
+String get newsTime {
+    final now = DateTime.now();
+    String time = "";
+    if (date != null) {
+      final difference = now.difference(date!);
+      if (difference.inDays < 1) {
+        // Mostra i minuti trascorsi tra now e date.day
+        final minutes = difference.inMinutes;
+        time = '$minutes${$strings.minutes_abbreviate}';
+      } else if (difference.inDays < 7) {
+        // Mostra i giorni trascorsi
+        final days = difference.inDays;
+        time = '$days${$strings.days_abbreviate}';
+      } else {
+        // Mostra la data completa
+        time = DateFormat("yyyy-MM-dd").format(date!);
+      }
+    }
+    return time;
+  }
