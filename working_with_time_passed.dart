109c5147b20738116eb8b361d677cbce117e3727
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2023-09-07 06:53:03 +0200

    best version

--- working_with_time_passed.dart
+++ working_with_time_passed.dart
@@ -56,3 +56,46 @@ String get newsTime {
     }
     return time;
   }
+
+//Best version
+String get newsTime {
+  final now = DateTime.now();
+  String time = "";
+
+  if (date != null) {
+    // Calculate the time difference between now and the provided date
+    final difference = now.difference(date!);
+
+    if (difference.inDays < 1) {
+      // If the difference is less than 1 day, show the minutes and hours (if applicable)
+      final minutes = difference.inMinutes;
+
+      if (minutes > 60) {
+        // Show hours and remaining minutes if the difference is more than 1 hour
+        final hours = (minutes / 60).floor();
+        final remainingMinutes = minutes % 60;
+
+        if (remainingMinutes == 0) {
+          // If there are no remaining minutes, display only the hours
+          time = '${hours}${$strings.hours_abbreviate}';
+        } else {
+          // Display both hours and minutes in the format: "3h 04m"
+          time = '${hours}h${remainingMinutes.toString().padLeft(2, '0')}${$strings.minutes_abbreviate}';
+        }
+      } else {
+        // Display minutes if the difference is less than 1 hour
+        time = '$minutes${$strings.minutes_abbreviate}';
+      }
+    } else if (difference.inDays < 7) {
+      // If the difference is less than 7 days, show the number of days
+      final days = difference.inDays;
+      time = '$days${$strings.days_abbreviate}';
+    } else {
+      // If the difference is more than 7 days, show the complete date
+      time = DateFormat("yyyy-MM-dd").format(date!);
+    }
+  }
+
+  return time;
+}
+
